9c54467d9889eb8848241065d9354166e135e55d
Gil <gil@civity.nl>
2019-03-19 12:16:21 +0100

    remove debug logging

--- ckanext/keycloaksynch/logic/action/update.py
+++ ckanext/keycloaksynch/logic/action/update.py
@@ -136,9 +136,6 @@ def _kc_usr_list_reformat(kc_user_list):
 def _update_ckan_users_based_on_kc_user_list(context, kc_user_list):
     # print 'Make changes on CKAN based on Keycloak User list'
     keycloack_username_list = list()
-    for kc_user in kc_user_list:
-        log.info('Synchronizing Keycloak user "{0}" in CKAN'.format(
-            kc_user['name']))
 
     for kc_user in kc_user_list:
         log.info('Synchronizing Keycloak user "{0}" in CKAN'.format(kc_user['name']))
